580484123d4ea68296b952354cb750745ee06811
Orlandaquisi <124109146+Orlandaquisi@users.noreply.github.com>
2023-03-06 23:42:16 -0600

    Add files via upload
    nuevo archivo

---
+++ proyectooo.java
@@ -0,0 +1,29 @@
+public class  vehiculo {
+     privateString name;
+     privateString marca;
+     privateString modelo;
+    privateString color;
+    
+    }
+     vehiculo(String name, String marca, String modelo, String color){
+        setName(name);
+        setMarca(marca);
+        setModelo(modelo);
+        setColor(color);
+        
+        }  
+
+        public void setName(String n){name = n;}
+        public void setMarca(String a){marca = a;}
+        public void setmModelo(String b){modelo = b;}
+        public void setColor(color h){color = h;}
+
+        public String getName(){return name;}
+        public String getMarca(){return marca;}
+        public String getModelo(){return modelo}
+        public String getColor(){return color;}
+
+        public void mostrarVehiculo (){
+        
+        
+        }
